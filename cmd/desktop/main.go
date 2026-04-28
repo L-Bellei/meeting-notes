af91d84fcd4246ddf7a0ee7152595a807b861418
Leonardo Bellei <lbellei@vitait.com>
2026-04-28 16:14:21 -0300

    refactor(desktop): move go:embed to assets.go

--- cmd/desktop/main.go
+++ cmd/desktop/main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"embed"
 	"log"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
-
-//go:embed all:frontend/dist
-var assets embed.FS
 
 func main() {
 	app := NewApp()
